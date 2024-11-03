54732d3015c0d157900db3bef5066c00156e6cf9
Yosodog <yosodog@gmail.com>
2024-11-02 22:46:20 -0500

    Show nation flag as user image when logged in

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 
 class User extends Authenticatable
 {
@@ -45,5 +46,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function nation()
+    {
+        return $this->hasOne(Nations::class, "id", "nation_id");
     }
 }
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -72,8 +72,8 @@
                     <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                alt="{{ Auth::user()->nation->leader_name }} Flag"
+                                src="{{ Auth::user()->nation->flag }}" />
                         </div>
                     </div>
                     <ul
