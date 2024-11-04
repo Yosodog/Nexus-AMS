b8fb6a32cac7693802f1733389507d35ac7ee4eb
Yosodog <yosodog@gmail.com>
2024-11-03 19:22:00 -0600

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountsController;
 
 Route::get('/', function () {
     return view('home');
 })->name("home");
+
+Route::middleware(['auth'])->group(function () {
+
+    // Account Routes
+    Route::get("/accounts", [AccountsController::class, 'index'])->name("accounts");
+    Route::post('accounts/transfer', [AccountsController::class, 'transfer'])
+        ->name('accounts.transfer');
+
+    Route::get("/accounts/{id}", [AccountsController::class, 'viewAccount'])->name("accounts.view");
+
+});
