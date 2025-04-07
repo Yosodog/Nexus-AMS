b4e9b5d8bcca59b49dc80f5df141af92ed04dad9
Yosodog <yosodog@gmail.com>
2025-04-07 18:20:12 -0500

    Install pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,13 @@ use App\Broadcasting\PWMessageChannel;
 use App\Models\CityGrantRequest;
 use App\Models\Loans;
 use App\Models\Nations;
+use App\Models\User;
 use App\Services\PWMessageService;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pulse\Facades\Pulse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +36,9 @@ class AppServiceProvider extends ServiceProvider
         Route::model('CityGrantRequest', CityGrantRequest::class);
         Route::model('Loans', Loans::class);
         Route::model('Nations', Nations::class);
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->is_admin;
+        });
     }
 }
