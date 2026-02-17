d0ba64c04b02cf4b4e102b168d82fe3bdc6b7e62
Yosodog <yosodog@gmail.com>
2026-02-16 22:25:51 -0600

    Fix navbar dropdown z-index

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -14,7 +14,7 @@
         ? Grants::query()->where('is_enabled', true)->orderBy('name')->get()
         : collect();
 @endphp
-<div class="w-full bg-base-100">
+<div class="relative z-50 w-full bg-base-100">
     <div class="container mx-auto px-2 sm:px-4">
         <div class="navbar relative gap-1 sm:gap-2">
             <div class="navbar-start min-w-0 gap-1">
@@ -28,7 +28,7 @@
                     </div>
                     @if ($showMemberNavigation)
                         <ul tabindex="0"
-                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-52 p-2 shadow">
                             <li><a href="{{ route("accounts") }}">Accounts</a></li>
                             <li><a href="{{ route('market.index') }}">Market</a></li>
                             <li><a href="{{ route('audit.index') }}">Audits</a></li>
@@ -65,12 +65,12 @@
                         </ul>
                     @elseif($user)
                         <ul tabindex="0"
-                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-52 p-2 shadow">
                             <li><a href="{{ route('apply.show') }}">Apply</a></li>
                         </ul>
                     @else
                         <ul tabindex="0"
-                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-52 p-2 shadow">
                             <li><a href="{{ route('apply.show') }}">Apply</a></li>
                         </ul>
                     @endif
@@ -91,7 +91,7 @@
                                     class="cursor-pointer px-4 py-2 text-base-content hover:bg-base-200 rounded-md transition">
                                     Grants
                                 </summary>
-                                <ul class="absolute left-0 mt-2 w-64 menu bg-base-100 p-2 shadow rounded-box z-[1]">
+                                <ul class="absolute left-0 mt-2 w-64 menu bg-base-100 p-2 shadow rounded-box z-[60]">
                                     <li><a href="{{ route("grants.city") }}">City Grants</a></li>
                                     @foreach ($enabledGrants as $grant)
                                         <li>
@@ -109,7 +109,7 @@
                                     class="cursor-pointer px-4 py-2 text-base-content hover:bg-base-200 rounded-md transition">
                                     Defense
                                 </summary>
-                                <ul class="absolute left-0 mt-2 w-64 menu bg-base-100 p-2 shadow rounded-box z-[1]">
+                                <ul class="absolute left-0 mt-2 w-64 menu bg-base-100 p-2 shadow rounded-box z-[60]">
                                     <li><a href="{{ route("defense.counters") }}">Counter Finder</a></li>
                                     <li><a href="{{ route("defense.intel") }}">Intel Library</a></li>
                                     <li><a href="{{ route("defense.war-aid") }}">War Aid</a></li>
@@ -150,7 +150,7 @@
                                 d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
                         </svg>
                     </div>
-                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[60] p-2 shadow bg-base-100 rounded-box w-52">
                         <li>
                             <a data-set-theme="light">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2 fill-current" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
@@ -181,7 +181,7 @@
                             </div>
                         </div>
                         <ul tabindex="0"
-                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-52 p-2 shadow">
                             <li><a href="{{ route("user.dashboard") }}">Dashboard</a></li>
                             <li><a href="{{ route("user.settings") }}">Settings</a></li>
                             @if ($user->is_admin)
@@ -210,7 +210,7 @@
                             </svg>
                         </div>
                         <ul tabindex="0"
-                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-44 p-2 shadow">
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-44 p-2 shadow">
                             <li><a href="{{ route("login") }}">Login</a></li>
                             <li><a href="{{ route("register") }}">Register</a></li>
                         </ul>
