084b3a1bb3924a5d4bb45bee346b9f44b348ed9e
Yosodog <yosodog@gmail.com>
2026-02-22 18:35:04 -0600

    Fix backup disk selection when S3 bucket is missing

--- config/backup.php
+++ config/backup.php
@@ -4,6 +4,10 @@ use Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy;
 use Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays;
 use Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes;
 
+$defaultBackupDisks = env('AWS_BUCKET') ? ['s3'] : ['local'];
+$configuredBackupDisks = array_values(array_filter(array_map('trim', explode(',', (string) env('BACKUP_DESTINATION_DISKS', implode(',', $defaultBackupDisks))))));
+$backupDisks = $configuredBackupDisks === [] ? $defaultBackupDisks : $configuredBackupDisks;
+
 return [
     'backup' => [
         'name' => env('APP_NAME', 'laravel-backup'),
@@ -32,9 +36,7 @@ return [
             'compression_method' => ZipArchive::CM_DEFAULT,
             'compression_level' => 9,
             'filename_prefix' => '',
-            'disks' => [
-                's3',
-            ],
+            'disks' => $backupDisks,
             'continue_on_failure' => false,
         ],
         'temporary_directory' => storage_path('app/backup-temp'),
@@ -51,9 +53,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => [
-                's3',
-            ],
+            'disks' => $backupDisks,
             'health_checks' => [
                 MaximumAgeInDays::class => 1,
                 MaximumStorageInMegabytes::class => 102400,
